dbf1d0e3c26338285fa850cd5b8dfbf57dbb35cd
AussyWoo <masterausli@gmail.com>
2021-03-19 16:46:18 -0600

    added basic joystick functionality

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,24 +7,35 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMSparkMax;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax leftMotor1 = new PWMSparkMax(1);
-  private final PWMSparkMax leftMotor2 = new PWMSparkMax(2);
-  private final PWMSparkMax rightMotor1 = new PWMSparkMax(3);
-  private final PWMSparkMax rightMotor2 = new PWMSparkMax(4);
+  private final PWMSparkMax rightMotor1 = new PWMSparkMax(1);
+  private final PWMSparkMax rightMotor2 = new PWMSparkMax(2);
+  private final PWMSparkMax leftMotor1 = new PWMSparkMax(3);
+  private final PWMSparkMax leftMotor2 = new PWMSparkMax(4);
+  private final Joystick joy = new Joystick(0);
   //private final DifferentialDrive robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   //private final Joystick m_stick = new Joystick(0);
 
+  @Override
+  public void robotInit() {
+    joy.setXChannel(5);
+  }
+  
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
     //m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    leftMotor1.set(joy.getY());
+    leftMotor2.set(joy.getY());
+    rightMotor1.set(joy.getX() * -1);
+    rightMotor2.set(joy.getX() * -1);
   }
 }
