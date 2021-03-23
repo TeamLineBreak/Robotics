7824487ce5814dd8fce2a60686630dfba55397f5
GiGodN <noahmjohn@gmail.com>
2021-03-23 11:23:25 -0600

    added build.* to git ignor and commented our code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,48 +9,62 @@ import edu.wpi.first.wpilibj.PWMSparkMax;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
- * arcade steering.
+ * This is the main class of the robot code, most of the code should be written or called here.
  */
 public class Robot extends TimedRobot {
   private final PWMSparkMax rightMotor1 = new PWMSparkMax(1);
   private final PWMSparkMax rightMotor2 = new PWMSparkMax(2);
   private final PWMSparkMax leftMotor1 = new PWMSparkMax(3);
   private final PWMSparkMax leftMotor2 = new PWMSparkMax(4);
-  private final Joystick joy = new Joystick(0);
+  private Joystick joy;
   private int mult;
   private double turnSpeed;
-  //private final DifferentialDrive robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-  //private final Joystick m_stick = new Joystick(0);
 
   @Override
   public void robotInit() {
+    //initalize the joystick
+    joy = new Joystick(0);
+    //set the robot heading to backwards, which is the battery side
     mult = -1;
+    //reduce the turn speed by 43 percent to increace control
     turnSpeed = .57;
+    //sets z axis to the right bumpber for the tank drve.
     joy.setZChannel(3);
   }
   
   @Override
   public void teleopPeriodic() {
-    // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    //m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    //if rb is pressed then switch the robot forward direction
     if(joy.getRawButtonPressed(6)) {
       mult *= -1;
     }
+    //if lb is pressed change increas the turn speed to make sharper turns while moving and rereduce the turnspeed on release.
     if(joy.getRawButtonPressed(5)) {
       turnSpeed = .85;
     }
     if(joy.getRawButtonReleased(5)) {
       turnSpeed = .57;
     }
+    //move the robot
+    tankDrive(joy, mult, turnSpeed);
+  }
+
+  /**
+   * Method to drive the robot, uses one controler axis as the turning input and another to control the robot speed.
+   * The control z axis must be set to the axis needed to control the speed, and the controler x axis controls the rotation.
+   * @param mult is the multiplier for the entire robot speed, mainly used to change the robots forward direction, can also be used for precice movment.
+   * @param turnSpeed the multiplier for the robots turn speed, cannot excied 1, used to slow the turning of the robot.
+   * @param joy the controler to be used as the control to control the robot. 
+   */
+  public void tankDrive(Joystick joy, int mult, double turnSpeed) {
+    //driving code for going forward.
     if(joy.getRawAxis(2) == 0 ^ mult == 1) {
       leftMotor1.set(((joy.getZ() - joy.getRawAxis(2)) + joy.getX()*turnSpeed) * mult);
       leftMotor2.set(((joy.getZ() - joy.getRawAxis(2)) + joy.getX()*turnSpeed) * mult);
       rightMotor1.set(((joy.getZ() - joy.getRawAxis(2))  - joy.getX()*turnSpeed) * -1 * mult);
       rightMotor2.set(((joy.getZ() - joy.getRawAxis(2))  - joy.getX()*turnSpeed) * -1 * mult);
     }
+    //driving code for going backwards
     else {
       leftMotor1.set(((joy.getZ() - joy.getRawAxis(2)) - joy.getX()*turnSpeed) * mult);
       leftMotor2.set(((joy.getZ() - joy.getRawAxis(2)) - joy.getX()*turnSpeed) * mult);
